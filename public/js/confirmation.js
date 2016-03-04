13a5773b9bf4ea758e818b2e71d2243ce7bfdec3
Phong <pdt004@ucsd.edu>
2016-03-03 20:03:30 -0800

    modified confirmation

--- public/js/confirmation.js
+++ public/js/confirmation.js
@@ -63,9 +63,10 @@ function prompt(confirm){
 			});*/
 			swal({title: 'Sweet!',html: 'Redirecting you to PayPal for completion.<br>Be aware of blocked pop-ups.', imageUrl: 'images/thumbs-up.jpg',animation: true });
 			setTimeout(function () {
+				ga('send', 'event', 'ok_button', 'click');	
 				window.open('https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=LWHJW82U99LPU&lc=US&item_name=UCSD%20Permits&amount=1%2e25&currency_code=USD&button_subtype=services&no_note=0&cn=Add%20special%20instructions%20to%20the%20seller%3a&no_shipping=2&tax_rate=0%2e000&shipping=0%2e00&bn=PP%2dBuyNowBF%3abtn_paynowCC_LG%2egif%3aNonHosted, _blank');
     		}, 3500);	
-    			ga('send', 'event', 'ok_button', 'click');		
+	
 		} 	
 		else {     
 			swal("Declined!", "Transaction was not processed!", "error");   
